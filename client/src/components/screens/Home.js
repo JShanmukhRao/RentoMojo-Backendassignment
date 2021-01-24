882a643fa3d05fd202125953702004f004a421fe
J Shanmukh Rao <49684626+JShanmukhRao@users.noreply.github.com>
2021-01-25 01:50:36 +0530

    Update Home.js

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,43 +1,32 @@
-import React, { useState, useEffect, useContext, useStateCallback } from 'react'
-import M from "materialize-css";
+import React, { useContext, useEffect, useState } from 'react';
 import Card from './Card'
+import M from "materialize-css";
 import { UserContext } from "../../App";
 import { useHistory } from "react-router-dom";
-import Pagination from 'react-paginate'
-
+import Pagination from "../Hooks/usePagination"
 const Home = () => {
-    const [search, setSearch] = useState("");
-    const [searchValue, setSearchValue] = useState("");
-    const contactContext = useContext(UserContext);
-    const { dispatch, ans } = contactContext;
-    const [standedData, setStandedData] = useState([]);
-    const [data, setData] = useState([]);
-    const [offset, setOffset] = useState(1);
-    const [perPage, setPerPage] = useState(4);
-    const [currPage, setCurrPage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-
-
-
+    
+    
     const history = useHistory();
-
+    const [standedData, setStandedData] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
+    const [page, setPage] = useState(1);
+    const [data, setData] = useState([]);
+    const [final, setFinal] = useState([])
+    const contactContext = useContext(UserContext);
+    const { dispatch } = contactContext;
     useEffect(() => {
-
         fetch('/allcontacts', {
             headers: {
                 "authorization": "Bearer " + localStorage.getItem("jwt")
             }
         }).then(res => res.json())
             .then(result => {
-
-
                 setStandedData(result.contacts);
-                setPageCount(Math.ceil(result.contacts.length / perPage));
-                var slice = result.contacts.slice(offset, offset + perPage);
-
-                setData(slice)
-
+                setTotalPages(Math.ceil(result.contacts.length / 10));
+                setData(result.contacts)
             })
+
     }, [])
     const deleteContact = (contactId) => {
         fetch(`/deletecontact/${contactId}`, {
@@ -47,10 +36,12 @@ const Home = () => {
             }
         }).then(res => res.json())
             .then(result => {
-                const newData = data.filter(item => {
+                const newData = standedData.filter(item => {
                     return item._id !== result._id;
                 })
                 setData(newData)
+                setStandedData(newData)
+               
             }).catch(err => {
                 M.toast({ html: err, classes: "#c62828 red darken-3" });
             })
@@ -61,65 +52,42 @@ const Home = () => {
             history.push("/addcon")
         )
     }
-    const handlePageClick = (e) => {
-        const selectedPage = e.selected;
-        setOffset(selectedPage * perPage);
-        setCurrPage(selectedPage)
 
-        loadMoreData()
-    }
-    const loadMoreData = () => {
-        console.log("load")
-        const data = standedData;
-        var slice = data.slice(offset, offset + perPage);
-        setPageCount(Math.ceil(data.length / perPage));
-        setData(slice)
-
-    }
     const handleChange = e => {
-        setSearchValue(e.target.value)
         var formatedData = standedData;
         const newData = formatedData.filter(contact => {
             const regex = new RegExp(`${e.target.value}`, 'gi');
             return contact.name.match(regex) || contact.email.match(regex);
         })
-
         setData(newData)
-
     }
+    const handleClick = num => {
+        
+        const startIndex = (num -1) * 10;
+        const selected=standedData.slice(startIndex, startIndex + 10);
+setData(selected)
+    }
+   
+
 
     return (
         <div>
-
             <div style={{ margin: "auto", maxWidth: "500px" }}>
-                <input placeholder="Search" onChange={(e) => handleChange(e)} />
-
-
+                <input placeholder="Search" onChange={(e) => handleChange(e)}  />
             </div>
-            {
+            {data.map(user => (
+                <Card
+                    item={user}
+                    key={user._id}
+                    EditContact={(contact) => editContact(contact)}
+                    DeleteContact={(contactId) => deleteContact(contactId)}
 
+                />
+            ))}
+                        <Pagination totalPages={totalPages} handleClick={handleClick} />
 
-                data.map(item => {
-                    return (
-                        <Card item={item}
-                            DeleteContact={(contactId) => deleteContact(contactId)}
-                            EditContact={(contact) => editContact(contact)}
-                        />
-                    )
-                })
-            }
-            <Pagination
-            previousClassName={"<"}
-            nextClassName={">"}
-            pageCount={pageCount}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={2}
-            containerClassName={"pagination"}
-            onPageChange={handlePageClick}
-            activeClassName={"active"}
-
-            />
         </div>
-    );
+    )
+
 }
 export default Home;
